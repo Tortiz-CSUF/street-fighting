b18f3f02d96f3d236e759500285bc0bb3149bfa8
Tortiz-CSUF <thomasortiz@csu.fullerton.edu>
2026-03-24 22:49:34 +0000

    street-fighting_Interim_13

--- enemy.gd
+++ enemy.gd
@@ -19,17 +19,22 @@ var knockback_velocity := Vector2.ZERO
 
 func _ready() -> void:
 	damage_receiver.damage_received.connect(on_receive_damage.bind())
+	animation_player.animation_finished.connect(on_animation_finished.bind())
 
 func _process(delta: float) -> void:
 	if player == null:
 		return
-	
+			
 	handle_movement()
 	handle_animation()
 	flip_sprites()
 	move_and_slide()
 	
 func handle_movement() -> void:
+	if state == State.HURT:
+		velocity = knockback_velocity
+		return
+	
 	var target := player.global_position + slot_offset
 	var distance := global_position.distance_to(target)
 	
@@ -49,9 +54,16 @@ func handle_animation() -> void:
 		anim_name = "idle"
 	elif state == State.WALK:
 		anim_name = "walk"
+	elif  state == State.HURT:
+		anim_name = "hurt"
 		
 	if animation_player.current_animation != anim_name:
 		animation_player.play(anim_name)
+
+func on_animation_finished(anim_name: String) -> void:
+	if anim_name == "hurt":
+		state = State.IDLE
+		knockback_velocity = Vector2.ZERO
 	
 func flip_sprites() -> void:
 	if player == null:
