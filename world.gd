e776aa9bd603de7b59ad9df0dfef5108e4a0c2b6
Tortiz-CSUF <thomasortiz@csu.fullerton.edu>
2026-03-24 21:59:15 +0000

    street-fighting_Interim_4

--- world.gd
+++ world.gd
@@ -3,6 +3,9 @@ extends Node2D
 @onready var player := $ActorsContainer/Player
 @onready var camera := $Camera
 
+func _ready() -> void:
+	for enemy in get_tree().get_nodes_in_group("enemies"):
+		enemy.player = player	
 func _process(delta: float) -> void:
 	if player.position.x > camera.position.x:
 		camera.position.x = player.position.x
